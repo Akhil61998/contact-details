a125ebf7671544784aeb9b62b30c702d5cad67b7
Akhil61998 <akhilkhyadad1357@gmail.com>
2025-01-16 08:58:23 +0530

    first commit

---
+++ contact-details/src/test/java/com/hello/contact_details/ContactDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.contact_details;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
